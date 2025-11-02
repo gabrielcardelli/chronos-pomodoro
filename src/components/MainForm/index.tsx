9504aa626d94ec05ec405a154966a336989a4a61
Gabriel <gabrielcastilho.cardelli@gmail.com>
2025-11-02 12:06:40 -0300

    Project class 65

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -17,6 +17,26 @@ export function MainForm() {
 
   const taskNameInput = useRef<HTMLInputElement>(null);
 
+  function handleInterruptTask() {
+    setState(prevState => {
+      return {
+        ...prevState,
+        activeTask: null,
+        secondsRemaining: 0,
+        formattedSecondsRemaining: '00:00',
+        tasks: prevState.tasks.map(task => {
+          if (task.id === prevState.activeTask?.id) {
+            return {
+              ...task,
+              interuptDate: Date.now(),
+            };
+          }
+          return task;
+        }),
+      };
+    });
+  }
+
   function handleCreateNewTask(event: React.FormEvent) {
     event.preventDefault();
     if (taskNameInput.current == null) return;
@@ -65,6 +85,7 @@ export function MainForm() {
           type='text'
           //   onChange={e => setTaskName(e.target.value)}
           ref={taskNameInput}
+          disabled={!!state.activeTask}
         />
       </div>
       <div className='formRow'>
@@ -76,8 +97,27 @@ export function MainForm() {
         </div>
       )}
       <div className='formRow'>
-        <DefaultButton icon={<PlayCircleIcon />} />
-        <DefaultButton icon={<StopCircleIcon />} color='red' />
+        {!state.activeTask && (
+          <DefaultButton
+            aria-label='Iniciar nova tarefa'
+            title='Iniciar nova tarefa'
+            type='submit'
+            icon={<PlayCircleIcon />}
+            key='btnSubmit'
+          />
+        )}
+
+        {state.activeTask && (
+          <DefaultButton
+            aria-label='Interromper tarefa atual'
+            title='Interromper tarefa atual'
+            type='button'
+            color='red'
+            onClick={handleInterruptTask}
+            icon={<StopCircleIcon />}
+            key='btnInterrupt'
+          />
+        )}
       </div>
     </form>
   );
--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer, useState } from 'react';
 import { initialTaskState } from './initialTaskState';
 import { TaskContext } from './TaskContext';
 
@@ -9,13 +9,59 @@ type TaskContextProviderProps = {
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
   const [state, setState] = useState(initialTaskState);
 
-  useEffect(() => {
-    console.log(state);
-  }, [state]);
+  type ActionType = {
+    type: string;
+    payload?: any;
+  };
+
+  const [myState, dispatch] = useReducer(
+    (state, action: ActionType) => {
+      console.log(action);
+      console.log(state);
+
+      switch (action.type) {
+        case 'INCREMENT': {
+          if (!action.payload) return state;
+
+          return {
+            ...state,
+            secondsRemaining: state.secondsRemaining + action.payload,
+          };
+        }
+        case 'DECREMENT': {
+          if (!action.payload) return state;
+
+          return {
+            ...state,
+            secondsRemaining: state.secondsRemaining - action.payload,
+          };
+        }
+        case 'RESET':
+          return { ...state, secondsRemaining: 0 };
+      }
+
+      return state;
+    },
+    { secondsRemaining: 0 },
+  );
+
+  // useEffect(() => {
+  //   console.log(state);
+  // }, [state]);
 
   return (
     <TaskContext.Provider value={{ state, setState }}>
-      {children}
+      <h1>O estado é : {JSON.stringify(myState)}</h1>
+      <button onClick={() => dispatch({ type: 'INCREMENT', payload: 10 })}>
+        Incrementar + 10
+      </button>
+      <button onClick={() => dispatch({ type: 'INCREMENT', payload: 20 })}>
+        Incrementar + 20
+      </button>
+      <button onClick={() => dispatch({ type: 'DECREMENT', payload: 50 })}>
+        Decrementar - 50
+      </button>
+      <button onClick={() => dispatch({ type: 'RESET' })}>RESET</button>
     </TaskContext.Provider>
   );
 }
