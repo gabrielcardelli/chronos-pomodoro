088edfe57293ab0ff2fad8404f87cb2a6dd30e54
Gabriel <gabrielcastilho.cardelli@gmail.com>
2025-11-10 23:57:12 -0300

    project until class 76

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useReducer, useState } from 'react';
+import { useEffect, useReducer, useRef, useState } from 'react';
 import { initialTaskState } from './initialTaskState';
 import { TaskContext } from './TaskContext';
 import { taskReducer } from './taskReducer';
 import { TaskActionTypes } from './taskActions';
 import { TimeWorkerManager } from '../../workers/TimeWorkManager';
+import { loadBeep } from '../../utils/loadBeep';
 
 type TaskContextProviderProps = {
   children: React.ReactNode;
@@ -12,12 +13,20 @@ type TaskContextProviderProps = {
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
   const [state, dispatch] = useReducer(taskReducer, initialTaskState);
 
+  let playBeepRef = useRef<() => void | null>(null);
+
   const worker = TimeWorkerManager.getInstance();
 
   worker.onmessage(e => {
     const countDownSeconds = e.data as number;
 
     if (countDownSeconds <= 0) {
+      if (playBeepRef.current) {
+        console.log('tocando audio');
+        playBeepRef.current();
+        playBeepRef.current = null;
+      }
+
       dispatch({
         type: TaskActionTypes.COMPLETE_TASK,
       });
@@ -36,11 +45,20 @@ export function TaskContextProvider({ children }: TaskContextProviderProps) {
     console.log(state);
     if (!state.activeTask) {
       worker.terminate();
-      console.log('Workert terminado por falta de active Taks');
     }
 
     worker.postMessage(state);
   }, [worker, state]);
+
+  useEffect(() => {
+    if (state.activeTask && playBeepRef.current === null) {
+      console.log('Carregando audio');
+      playBeepRef.current = loadBeep();
+    } else {
+      console.log('Zerando o audio');
+      playBeepRef.current = null;
+    }
+  }, [state.activeTask]);
 
   return (
     <TaskContext.Provider value={{ state, dispatch }}>
